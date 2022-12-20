5ce03b6aef712586acc2e8d12b17922432d295eb
Terry Mak <tymc447@gmail.com>
2022-12-20 21:33:01 +0800

    production build

--- build/index.js
+++ build/index.js
@@ -3,14 +3,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
+const loginRouter_1 = __importDefault(require("./router/loginRouter"));
+const middleware_1 = require("./utils/middleware");
+const tokenRouter_1 = __importDefault(require("./router/tokenRouter"));
+// import mongoose from "mongoose";
 const app = (0, express_1.default)();
+app.use(express_1.default.static("frontend-build"));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-const PORT = 3000;
-app.get("/ping", (_req, res) => {
-    console.log("someone pinged here");
-    res.send("pong");
+app.use(middleware_1.requestLogger);
+mongoose_1.default
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+    console.log("connected to MongoDB");
+})
+    .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
 });
+app.use("/api/users", tokenRouter_1.default);
+app.use("/api/login", loginRouter_1.default);
+app.use(middleware_1.unknownEndpoint);
+app.use(middleware_1.errorHandler);
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- index.ts
+++ index.ts
@@ -12,11 +12,10 @@ import tokenRouter from "./router/tokenRouter";
 // import mongoose from "mongoose";
 
 const app = express();
+app.use(express.static("frontend-build"));
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
-
-const PORT = 3001;
 
 mongoose
   .connect(process.env.MONGO_URI!)
@@ -31,6 +30,8 @@ app.use("/api/users", tokenRouter);
 app.use("/api/login", loginRouter);
 app.use(unknownEndpoint);
 app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
